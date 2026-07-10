6414660dcc8c5753f4479e0d446af80a45e31b72
Victor Celi <vcelir2>
2026-07-10 05:27:42 -0500

    feat: agregar boton para limpiar filtros

--- src/components/OrderFilters.jsx
+++ src/components/OrderFilters.jsx
@@ -1,6 +1,9 @@
+import PropTypes from 'prop-types';
 import { FILTER_ALL_STATUS, ORDER_STATUS_OPTIONS } from '../utils/orderUtils.js';
 
 function OrderFilters({ searchTerm, statusFilter, onSearchChange, onStatusChange, onClearFilters }) {
+  const isClean = !searchTerm.trim() && statusFilter === FILTER_ALL_STATUS;
+
   return (
     <div className="filters" aria-label="Filtros de pedidos">
       <div className="filter-field">
@@ -26,11 +29,24 @@ function OrderFilters({ searchTerm, statusFilter, onSearchChange, onStatusChange
         </select>
       </div>
 
-      <button className="secondary-button" type="button" onClick={onClearFilters}>
+      <button
+        className="secondary-button"
+        type="button"
+        onClick={onClearFilters}
+        disabled={isClean}
+      >
         Limpiar filtros
       </button>
     </div>
   );
 }
+
+OrderFilters.propTypes = {
+  searchTerm: PropTypes.string.isRequired,
+  statusFilter: PropTypes.string.isRequired,
+  onSearchChange: PropTypes.func.isRequired,
+  onStatusChange: PropTypes.func.isRequired,
+  onClearFilters: PropTypes.func.isRequired,
+};
 
 export default OrderFilters;
